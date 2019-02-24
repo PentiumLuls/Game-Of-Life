27a0e88d640f7c7f2d9bf47d2c2e91b9445f4889
Maksym Volkov <maksvolkov26p2@gmail.com>
2019-02-24 22:11:05 +0200

    pause and custom paint on mouse click

--- js/game.js
+++ js/game.js
@@ -4,17 +4,27 @@ let canvasHeight;
 let size;
 let isGameOver;
 let grid;
+let generation;
+const keyMap = {
+    32: 'space'
+};
+const pressedKeys = {
+    space: false,
+}
 
 function run() {
     setup();
     const canvas = document.getElementById('canvas');
     canvas.addEventListener("mouseup", handleCanvasClick, false);
+    window.addEventListener("keyup", keyup, false);
     window.requestAnimationFrame(gameLoop);
 }
 
 function gameLoop() {
-    update();
     draw();
+    if (pressedKeys.space === false)
+        update();
+    window.requestAnimationFrame(gameLoop);
 }
 
 function setup() {
@@ -25,16 +35,18 @@ function setup() {
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     grid = create2DArray(size);
+    generation = 1;
     isGameOver = false;
-    for(var x = 0; x < size.cols; x++) {
+    for (var x = 0; x < size.cols; x++) {
         for (var y = 0; y < size.rows; y++) {
             grid[x][y] = new Cell(x * cellSize, y * cellSize);
         }
     }
+    initializeFirstBoardState();
 }
 
 function update() {
-    for(var x = 0; x < size.cols; x++) {
+    for (var x = 0; x < size.cols; x++) {
         for (var y = 0; y < size.rows; y++) {
             const neighbors = countAliveNeighbors(x, y);
             if (grid[x][y].alive === true) {
@@ -53,34 +65,50 @@ function update() {
         }
     }
 
-    for(var x = 0; x < size.cols; x++) {
+    for (var x = 0; x < size.cols; x++) {
         for (var y = 0; y < size.rows; y++) {
             grid[x][y].alive = grid[x][y].next;
         }
     }
+    generation++;
 }
 
 function draw() {
     const canvas = document.getElementById('canvas');
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    for(var x = 0; x < size.cols; x++) {
+    for (var x = 0; x < size.cols; x++) {
         for (var y = 0; y < size.rows; y++) {
             grid[x][y].draw(ctx, cellSize);
         }
     }
+    ctx.font = "24px Georgia";
+    ctx.fillStyle = 'red';
+    ctx.fillText(generation, 30, 30);
+}
+
+function keyup(event) {
+    var key = keyMap[event.keyCode];
+    pressedKeys[key] = !pressedKeys[key];
 }
 
 function handleCanvasClick(event) {
-    btnCode = event.button;
+    const btnCode = event.button;
 
     const x = event.offsetX;
     const y = event.offsetY;
     const cellX = Math.floor(x / cellSize);
     const cellY = Math.floor(y / cellSize);
 
-
+    if (btnCode === 0) {
+        //LEFT CLICK
+        grid[cellX][cellY].alive = true;
+    } else if (btnCode === 2) {
+        //RIGHT CLICK
+        grid[cellX][cellY].alive = false;
+    }
 }
+
 function countAliveNeighbors(x, y) {
     var result = 0;
     for (var i = -1; i <= 1; i++) {
@@ -90,13 +118,21 @@ function countAliveNeighbors(x, y) {
                 if (grid[x + i][y + j].alive === true) {
                     result++;
                 }
-            } catch (e) {}
+            } catch (e) {
+            }
         }
     }
     return result;
 }
 
-
+function initializeFirstBoardState() {
+    //GLIDER
+    grid[1][2].alive = true;
+    grid[2][3].alive = true;
+    grid[3][3].alive = true;
+    grid[3][2].alive = true;
+    grid[3][1].alive = true;
+}
 
 function create2DArray(size) {
     const grid = new Array(size.cols);
