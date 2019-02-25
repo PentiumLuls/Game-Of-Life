7a36a19577cc206dee05235063983d9faa7d8c02
Maksym Volkov <maksvolkov26p2@gmail.com>
2019-02-25 15:17:43 +0200

    optimisation -> remake in .p5

--- js/game.js
+++ js/game.js
@@ -1,362 +1,255 @@
-let cellSize = 20;
-let canvasWidth;
-let canvasHeight;
-let size;
-let isGameOver;
+function make2DArray(cols, rows) {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
 let grid;
-let generation;
-let fps = 10;
+let cols;
+let rows;
+let resolution = 20;
 let boardWidth = 40;
 let boardHeight = 30;
-let loop;
-const keyMap = {
-    32: 'space'
-};
-const pressedKeys = {
-    space: false,
-};
 
-function run() {
-    setup(boardWidth, boardHeight, cellSize);
-    const canvas = document.getElementById('canvas');
-    canvas.addEventListener("mouseup", handleCanvasClick, false);
-    window.addEventListener("keyup", keyup, false);
-    loop = setInterval(gameLoop, 1000 / fps);
+function setup() {
+    createCanvas(boardWidth * resolution, boardHeight * resolution);
+    cols = width / resolution;
+    rows = height / resolution;
 
-    document.getElementById('board-width-input').oninput = function () {
-        if (this.value > 500) {
-            this.value = 500;
-        } else if (this.value < 1) {
-            this.value = 1;
-        }
-    };
-    document.getElementById('board-height-input').oninput = function () {
-        if (this.value > 500) {
-            this.value = 500;
-        } else if (this.value < 1) {
-            this.value = 1;
-        }
-    };
-    document.getElementById('board-cell-size-input').oninput = function () {
-        if (this.value > 100) {
-            this.value = 100;
-        } else if (this.value < 5) {
-            this.value = 5;
-        }
-    };
-    document.getElementById('speed-input').oninput = function () {
-        if (this.value > 100) {
-            this.value = 100;
-        } else if (this.value < 1) {
-            this.value = 1;
-        }
-    };
-}
-
-function gameLoop() {
-    draw();
-    if (pressedKeys.space === false)
-        update();
-}
-
-function setup(width, height, newCellSize) {
-    cellSize = newCellSize;
-    boardWidth = width;
-    boardHeight = height;
-
-    const canvas = document.getElementById('canvas');
-    canvasWidth = cellSize * boardWidth;
-    canvasHeight = cellSize * boardHeight;
-    size = {cols: canvasWidth / cellSize, rows: canvasHeight / cellSize};
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-    grid = create2DArray(size);
-    generation = 1;
-    isGameOver = false;
-    for (var x = 0; x < size.cols; x++) {
-        for (var y = 0; y < size.rows; y++) {
-            grid[x][y] = new Cell(x * cellSize, y * cellSize);
+    grid = make2DArray(cols, rows);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
         }
     }
-}
-
-function update() {
-    for (var x = 0; x < size.cols; x++) {
-        for (var y = 0; y < size.rows; y++) {
-            const neighbors = countAliveNeighbors(x, y);
-            if (grid[x][y].alive === true) {
-                if (neighbors === 2 || neighbors === 3) {
-                    grid[x][y].next = true;
-                } else {
-                    grid[x][y].next = false;
-                }
-            } else {
-                if (neighbors === 3) {
-                    grid[x][y].next = true;
-                } else {
-                    grid[x][y].next = false;
-                }
-            }
-        }
-    }
-
-    for (var x = 0; x < size.cols; x++) {
-        for (var y = 0; y < size.rows; y++) {
-            grid[x][y].prev = grid[x][y].alive;
-            grid[x][y].alive = grid[x][y].next;
-        }
-    }
-    generation++;
 }
 
 function draw() {
-    const canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext("2d", {alpha: false});
-    for (var x = 0; x < size.cols; x++) {
-        for (var y = 0; y < size.rows; y++) {
-            if (grid[x][y].alive !== grid[x][y].prev || generation === 1)
-                grid[x][y].draw(ctx, cellSize);
-            else if (grid[x][y].needRedraw) {
-                grid[x][y].draw(ctx, cellSize);
-                grid[x][y].needRedraw = false;
+    background(0);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            if (grid[i][j] == 1) {
+                fill(255);
+                stroke(0);
+                rect(x, y, resolution - 1, resolution - 1);
             }
         }
     }
-    const generationCounterCanvas = document.getElementById('generationCounter');
-    var ctx2 = generationCounterCanvas.getContext("2d");
-    ctx2.clearRect(0, 0, 100, 50);
-    ctx2.font = "24px Georgia";
-    ctx2.fillStyle = 'red';
-    ctx2.fillText(generation, 10, 30);
+
+    let next = make2DArray(cols, rows);
+    // Compute next based on grid
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let state = grid[i][j];
+            // Count live neighbors!
+            let sum = 0;
+            let neighbors = countNeighbors(grid, i, j);
+
+            if (state == 0 && neighbors == 3) {
+                next[i][j] = 1;
+            } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+                next[i][j] = 0;
+            } else {
+                next[i][j] = state;
+            }
+        }
+    }
+    grid = next;
 }
 
-function keyup(event) {
-    var key = keyMap[event.keyCode];
-    pressedKeys[key] = !pressedKeys[key];
+function countNeighbors(grid, x, y) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            let col = (x + i + cols) % cols;
+            let row = (y + j + rows) % rows;
+            sum += grid[col][row];
+        }
+    }
+    sum -= grid[x][y];
+    return sum;
 }
 
-function handleCanvasClick(event) {
-    const btnCode = event.button;
-
-    const x = event.offsetX;
-    const y = event.offsetY;
-    const cellX = Math.floor(x / cellSize);
-    const cellY = Math.floor(y / cellSize);
-
-    if (btnCode === 0) {
-        //LEFT CLICK
-        grid[cellX][cellY].alive = !grid[cellX][cellY].alive;
-        grid[cellX][cellY].needRedraw = true;
-    } else if (btnCode === 2) {
-        //RIGHT CLICK
-        //PLACE DIFFERENT STRUCTURE
-
-        grid[cellX][cellY].needRedraw = true;
+function clearBoard() {
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = 0;
+        }
     }
 }
 
 function applySettings() {
-    const width = document.getElementById('board-width-input').value;
-    const height = document.getElementById('board-height-input').value;
+    const newWidth = document.getElementById('board-width-input').value;
+    const newHeight = document.getElementById('board-height-input').value;
     const newCellSize = document.getElementById('board-cell-size-input').value;
-    fps = document.getElementById('speed-input').value;
 
-    if (!(width === boardWidth && height === boardHeight && newCellSize === cellSize)) {
-        setup(width, height, newCellSize);
+    if (!(newWidth === width && newHeight === height && newCellSize === resolution)) {
+        boardHeight = newHeight;
+        boardWidth = newWidth;
+        resolution = newCellSize;
+        setup();
     }
-    clearInterval(loop);
-    loop = setInterval(gameLoop, 1000 / fps);
-}
-
-function clearBoard() {
-    for (var x = 0; x < size.cols; x++) {
-        for (var y = 0; y < size.rows; y++) {
-            grid[x][y].alive = false;
-            grid[x][y].next = false;
-            grid[x][y].needRedraw = true;
-        }
-    }
-}
-
-function countAliveNeighbors(x, y) {
-    var result = 0;
-    for (var i = -1; i <= 1; i++) {
-        for (var j = -1; j <= 1; j++) {
-            try {
-                if (i === 0 && j === 0) continue;
-                if (grid[x + i][y + j].alive === true) {
-                    result++;
-                }
-            } catch (e) {
-            }
-        }
-    }
-    return result;
-}
-
-function create2DArray(size) {
-    const grid = new Array(size.cols);
-    for (var x = 0; x < size.cols; x++) {
-        grid[x] = new Array(size.rows);
-    }
-    return grid;
 }
 
 function handleStructureSelector() {
     clearBoard();
-    let x = Math.floor(boardWidth / 2);
-    let y = Math.floor(boardHeight / 2);
+    let x = Math.floor(cols / 2);
+    let y = Math.floor(rows / 2);
     switch (document.getElementById('structure-selector').value) {
         case 'clear': {
+            clearBoard();
             break;
         }
         case 'glider': {
             x -= 2;
             y -= 2;
-            grid[x][y+1].alive = true;
-            grid[x+1][y+2].alive = true;
-            grid[x+2][y+2].alive = true;
-            grid[x+2][y+1].alive = true;
-            grid[x+2][y].alive = true;
+            grid[x][y + 1] = 1;
+            grid[x + 1][y + 2] = 1;
+            grid[x + 2][y + 2] = 1;
+            grid[x + 2][y + 1] = 1;
+            grid[x + 2][y] = 1;
             break;
         }
         case 'small exploder': {
             x -= 2;
             y -= 2;
-            grid[x+1][y].alive = true;
-            grid[x][y+1].alive = true;
-            grid[x+1][y+1].alive = true;
-            grid[x+2][y+1].alive = true;
-            grid[x][y+2].alive = true;
-            grid[x+2][y+2].alive = true;
-            grid[x+1][y+3].alive = true;
+            grid[x + 1][y] = 1;
+            grid[x][y + 1] = 1;
+            grid[x + 1][y + 1] = 1;
+            grid[x + 2][y + 1] = 1;
+            grid[x][y + 2] = 1;
+            grid[x + 2][y + 2] = 1;
+            grid[x + 1][y + 3] = 1;
             break;
         }
         case 'exploder': {
             x -= 5;
             y -= 5;
-            grid[x][y].alive = true;
-            grid[x+2][y].alive = true;
-            grid[x+4][y].alive = true;
-            grid[x][y+1].alive = true;
-            grid[x+4][y+1].alive = true;
-            grid[x][y+2].alive = true;
-            grid[x+4][y+2].alive = true;
-            grid[x][y+3].alive = true;
-            grid[x+4][y+3].alive = true;
-            grid[x][y+4].alive = true;
-            grid[x+2][y+4].alive = true;
-            grid[x+4][y+4].alive = true;
+            grid[x][y] = 1;
+            grid[x + 2][y] = 1;
+            grid[x + 4][y] = 1;
+            grid[x][y + 1] = 1;
+            grid[x + 4][y + 1] = 1;
+            grid[x][y + 2] = 1;
+            grid[x + 4][y + 2] = 1;
+            grid[x][y + 3] = 1;
+            grid[x + 4][y + 3] = 1;
+            grid[x][y + 4] = 1;
+            grid[x + 2][y + 4] = 1;
+            grid[x + 4][y + 4] = 1;
             break;
         }
         case '10 cell row': {
             x -= 5;
-            grid[x][y].alive = true;
-            grid[x+1][y].alive = true;
-            grid[x+2][y].alive = true;
-            grid[x+3][y].alive = true;
-            grid[x+4][y].alive = true;
-            grid[x+5][y].alive = true;
-            grid[x+6][y].alive = true;
-            grid[x+7][y].alive = true;
-            grid[x+8][y].alive = true;
-            grid[x+9][y].alive = true;
+            grid[x][y] = 1;
+            grid[x + 1][y] = 1;
+            grid[x + 2][y] = 1;
+            grid[x + 3][y] = 1;
+            grid[x + 4][y] = 1;
+            grid[x + 5][y] = 1;
+            grid[x + 6][y] = 1;
+            grid[x + 7][y] = 1;
+            grid[x + 8][y] = 1;
+            grid[x + 9][y] = 1;
             break;
         }
         case 'lightweight spaceship': {
             x -= 3;
             y -= 3;
-            grid[x+1][y].alive = true;
-            grid[x+2][y].alive = true;
-            grid[x+3][y].alive = true;
-            grid[x+4][y].alive = true;
-            grid[x][y+1].alive = true;
-            grid[x+4][y+1].alive = true;
-            grid[x+4][y+2].alive = true;
-            grid[x][y+3].alive = true;
-            grid[x+3][y+3].alive = true;
+            grid[x + 1][y] = 1;
+            grid[x + 2][y] = 1;
+            grid[x + 3][y] = 1;
+            grid[x + 4][y] = 1;
+            grid[x][y + 1] = 1;
+            grid[x + 4][y + 1] = 1;
+            grid[x + 4][y + 2] = 1;
+            grid[x][y + 3] = 1;
+            grid[x + 3][y + 3] = 1;
             break;
         }
         case 'tumbler': {
             x -= 6;
             y -= 6;
-            grid[x+1][y].alive = true;
-            grid[x+2][y].alive = true;
-            grid[x+4][y].alive = true;
-            grid[x+5][y].alive = true;
+            grid[x + 1][y] = 1;
+            grid[x + 2][y] = 1;
+            grid[x + 4][y] = 1;
+            grid[x + 5][y] = 1;
 
-            grid[x+1][y+1].alive = true;
-            grid[x+2][y+1].alive = true;
-            grid[x+4][y+1].alive = true;
-            grid[x+5][y+1].alive = true;
+            grid[x + 1][y + 1] = 1;
+            grid[x + 2][y + 1] = 1;
+            grid[x + 4][y + 1] = 1;
+            grid[x + 5][y + 1] = 1;
 
-            grid[x+2][y+2].alive = true;
-            grid[x+4][y+2].alive = true;
+            grid[x + 2][y + 2] = 1;
+            grid[x + 4][y + 2] = 1;
 
-            grid[x][y+3].alive = true;
-            grid[x+2][y+3].alive = true;
-            grid[x+4][y+3].alive = true;
-            grid[x+6][y+3].alive = true;
+            grid[x][y + 3] = 1;
+            grid[x + 2][y + 3] = 1;
+            grid[x + 4][y + 3] = 1;
+            grid[x + 6][y + 3] = 1;
 
-            grid[x][y+4].alive = true;
-            grid[x+2][y+4].alive = true;
-            grid[x+4][y+4].alive = true;
-            grid[x+6][y+4].alive = true;
+            grid[x][y + 4] = 1;
+            grid[x + 2][y + 4] = 1;
+            grid[x + 4][y + 4] = 1;
+            grid[x + 6][y + 4] = 1;
 
-            grid[x][y+5].alive = true;
-            grid[x+1][y+5].alive = true;
-            grid[x+5][y+5].alive = true;
-            grid[x+6][y+5].alive = true;
+            grid[x][y + 5] = 1;
+            grid[x + 1][y + 5] = 1;
+            grid[x + 5][y + 5] = 1;
+            grid[x + 6][y + 5] = 1;
             break;
         }
 
         case 'gosper glider gun': {
             x -= 19;
             y -= 10;
-            grid[x][y+2].alive = true;
-            grid[x+1][y+2].alive = true;
-            grid[x][y+3].alive = true;
-            grid[x+1][y+3].alive = true;
+            grid[x][y + 2] = 1;
+            grid[x + 1][y + 2] = 1;
+            grid[x][y + 3] = 1;
+            grid[x + 1][y + 3] = 1;
 
-            grid[x+8][y+3].alive = true;
-            grid[x+8][y+4].alive = true;
-            grid[x+9][y+2].alive = true;
-            grid[x+9][y+4].alive = true;
-            grid[x+10][y+2].alive = true;
-            grid[x+10][y+3].alive = true;
+            grid[x + 8][y + 3] = 1;
+            grid[x + 8][y + 4] = 1;
+            grid[x + 9][y + 2] = 1;
+            grid[x + 9][y + 4] = 1;
+            grid[x + 10][y + 2] = 1;
+            grid[x + 10][y + 3] = 1;
 
-            grid[x+16][y+4].alive = true;
-            grid[x+16][y+5].alive = true;
-            grid[x+16][y+6].alive = true;
-            grid[x+17][y+4].alive = true;
-            grid[x+18][y+5].alive = true;
+            grid[x + 16][y + 4] = 1;
+            grid[x + 16][y + 5] = 1;
+            grid[x + 16][y + 6] = 1;
+            grid[x + 17][y + 4] = 1;
+            grid[x + 18][y + 5] = 1;
 
-            grid[x+22][y+1].alive = true;
-            grid[x+22][y+2].alive = true;
-            grid[x+23][y].alive = true;
-            grid[x+23][y+2].alive = true;
-            grid[x+24][y].alive = true;
-            grid[x+24][y+1].alive = true;
+            grid[x + 22][y + 1] = 1;
+            grid[x + 22][y + 2] = 1;
+            grid[x + 23][y] = 1;
+            grid[x + 23][y + 2] = 1;
+            grid[x + 24][y] = 1;
+            grid[x + 24][y + 1] = 1;
 
-            grid[x+24][y+12].alive = true;
-            grid[x+24][y+13].alive = true;
-            grid[x+25][y+12].alive = true;
-            grid[x+25][y+14].alive = true;
-            grid[x+26][y+12].alive = true;
+            grid[x + 24][y + 12] = 1;
+            grid[x + 24][y + 13] = 1;
+            grid[x + 25][y + 12] = 1;
+            grid[x + 25][y + 14] = 1;
+            grid[x + 26][y + 12] = 1;
 
-            grid[x+34][y].alive = true;
-            grid[x+34][y+1].alive = true;
-            grid[x+35][y].alive = true;
-            grid[x+35][y+1].alive = true;
+            grid[x + 34][y] = 1;
+            grid[x + 34][y + 1] = 1;
+            grid[x + 35][y] = 1;
+            grid[x + 35][y + 1] = 1;
 
-            grid[x+35][y+7].alive = true;
-            grid[x+35][y+8].alive = true;
-            grid[x+35][y+9].alive = true;
-            grid[x+36][y+7].alive = true;
-            grid[x+37][y+8].alive = true;
+            grid[x + 35][y + 7] = 1;
+            grid[x + 35][y + 8] = 1;
+            grid[x + 35][y + 9] = 1;
+            grid[x + 36][y + 7] = 1;
+            grid[x + 37][y + 8] = 1;
             break;
         }
     }
-}
 
-run();
+}
